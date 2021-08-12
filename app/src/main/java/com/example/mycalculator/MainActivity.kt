05576586783886132ab126be00acef70966ab50f
Mathias-Andre <19031965@myrp.edu.sg>
2021-08-12 23:07:30 +0800

    .

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,10 +2,62 @@ package com.example.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onAC(view: View){
+        btnAC.setOnClickListener {
+        }
+    }
+
+    fun onOperator(view: View){
+        btnDivide.setOnClickListener {
+        }
+        btnMultiply.setOnClickListener {
+        }
+        btnAdd.setOnClickListener {
+        }
+        btnSubtract.setOnClickListener {
+        }
+    }
+
+    fun onDigit(view: View){
+        btn0.setOnClickListener {
+        }
+        btn1.setOnClickListener {
+        }
+        btn2.setOnClickListener {
+        }
+        btn3.setOnClickListener {
+        }
+        btn4.setOnClickListener {
+        }
+        btn5.setOnClickListener {
+        }
+        btn6.setOnClickListener {
+        }
+        btn7.setOnClickListener {
+        }
+        btn8.setOnClickListener {
+        }
+        btn9.setOnClickListener {
+        }
+    }
+
+    fun onDot(view: View){
+        btnDot.setOnClickListener {
+        }
+    }
+
+    fun onEquals(view: View){
+        btnEquals.setOnClickListener {
+        }
+    }
+
 }
